49dd97fce23ebc2ba7236c269e329c5df1b57dff
Martin-Prusa <73097610+Martin-Prusa@users.noreply.github.com>
2021-05-17 08:23:59 +0200

    notEquals

--- src/test/java/cz/company/logic/LogicTest.java
+++ src/test/java/cz/company/logic/LogicTest.java
@@ -10,30 +10,40 @@ class LogicTest {
     @Test
     void plus() {
         assertEquals(5.5, l.plus(5d, 0.5));
+        assertNotEquals(0, l.plus(3, 2));
     }
 
     @Test
     void minus() {
         assertEquals(4.5, l.minus(5d, 0.5));
+        assertNotEquals(0, l.minus(3, 2));
+
     }
 
     @Test
     void times() {
         assertEquals(6, l.times(2, 3));
+        assertNotEquals(0, l.times(3, 2));
+
     }
 
     @Test
     void divided() {
         assertEquals(20, l.divided(100, 5));
+        assertNotEquals(0, l.divided(3, 2));
     }
 
     @Test
     void power() {
         assertEquals(8, l.power(2,3));
+        assertNotEquals(0, l.power(3, 2));
+
     }
 
     @Test
     void factorial() {
         assertEquals(120, l.factorial(5));
+        assertNotEquals(0, l.factorial(3));
+
     }
 }
